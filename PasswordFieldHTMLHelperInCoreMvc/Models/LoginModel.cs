2ce9847bb2e3fa55a91e3060bef115edcc2e8847
mukesh kumar <mukesh.1006jhr@outlook.com>
2023-10-22 04:17:55 +0530

    Add project files.

---
+++ PasswordFieldHTMLHelperInCoreMvc/Models/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PasswordFieldHTMLHelperInCoreMvc.Models
+{
+    public class LoginModel
+    {
+        public int LoginId { get; set; }
+        public string? LoginName { get; set; }
+        public string? LoginPassword { get; set; }
+    }
+}
